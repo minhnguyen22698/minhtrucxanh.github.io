7516a159b0b5f433169c167deea5d4b6382d2017
minhnguyen22698 <minhnguyen22698@gmail.com>
2021-04-24 11:32:43 +0700

    click on game unfinish

--- modules/Card.js
+++ modules/Card.js
@@ -4,25 +4,21 @@ import { Label } from '../lib/Label.js'
 import { cardFlipAnimate, cardZoomOutAnimate } from '../engine/Animate.js'
 var listClick = [];
 var parentNode = [];
-var score = 10000;
+var score = 10000
 var matched = 0;
-
 export class Card extends Node {
     constructor(src, index, value) {
         super()
-        this._initElement(src)
-        this._src = 'testing'
+        this._src = ''
+        if (src) this.src = src
         this._index = '';
         this._value = '';
         if (index) this.index = index
         if (value) this.value = value
         this._width = 50;
         this._height = 50;
-    }
-    _initElement(src) {
-        super._initElement();
-        this._initImage(src);
-        this._initCover();
+        this._initImage(src)
+        this._initCover(index)
     }
     get src() {
         return this._src
@@ -36,73 +32,97 @@ export class Card extends Node {
     set index(val) {
         this._index = val
     }
-    get index() {
+    get value() {
         return this._index
     }
     set value(val) {
         this._value = val
         this.ele.value = val
     }
-    _initCover() {
-        var label = new Label(this.index)
-        label.setText = this.index
-        this.addChild(label)
-        var cover = new Sprite('./img/cover.jpg')
-        cover.width = 100;
-        cover.height = 100;
-        cover.x = 100;
-        cover.y = 100;
+
+    // _initElement(src = this.src, index = this.index) {
+    //     console.log("init: " + src)
+    //     this._initImage(src);
+    //     this._initCover(index);
+    // }
+    _initCover(index) {
+        var cover = new Node()
+        cover.background = './img/cover.jpg';
+        cover.width = 150;
+        cover.height = 150;
+        // cover.border='5px solid gray'
         this.addChild(cover);
-        cover.on("mousedown", () => this.onClickCard(cover));
-    }
-    _initScore() {
-        var label = new Label('Score : ', 30, 'red')
-        label.setText = "Score: "
-        this.addChild(label)
+        //cover.on("mousedown", () => this.onClickCard(cover));
+        var label = new Label(this.index)
+        label.text = index;
+        label.fontColor = 'white'
+        label.fontSize = 30
+        label.x = cover.width / 2 - 10;
+        label.y = cover.width / 2 - 10;
+        cover.addChild(label)
     }
     _initImage(src) {
         var img = new Sprite(src)
-        img.width = 100;
-        img.height = 100;
-        img.x = 100;
-        img.y = 100;
-        img.scaleX = 0
-        this.addChild(img)
+        img.width = 150;
+        img.height = 150;
+        img.scaleX = 0;
+        this.addChild(img);
     }
     setScore(val) {
-        score = val
-        document.getElementById('score').innerHTML = score
-        if (matched == 10) {
-            alert('You win')
-        } else if (score == 0) {
-            alert('You lose')
+        var obj = {
+            value: score
         }
-    }
-
-    onClickCard(cover) {
-        if (listClick.length >= 2) { return }
-        cardFlipAnimate(cover, this.ele.children[0])
-        parentNode.push(this);
-        listClick.push(cover);
-        if (parentNode.length == 2) {
-            if (parentNode[0]._value === parentNode[1]._value) {
-                setTimeout(() => {
-                    cardZoomOutAnimate(parentNode[0].ele.children[0])
-                    cardZoomOutAnimate(parentNode[1].ele.children[0])
-                    parentNode = []; listClick = []
-                    matched++
-                    this.setScore(score + 1000)
-                }, 1000);
-            } else {
-
-                setTimeout(() => {
-                    cardFlipAnimate(parentNode[0].ele.children[0], listClick[0])
-                    cardFlipAnimate(parentNode[1].ele.children[0], listClick[1])
-                    parentNode = []; listClick = []
-                    this.setScore(score - 500)
-                }, 1500);
+        TweenLite.to(obj, 0.4, {
+            value: val,
+            roundProps: {
+                value: 20,
+            },
+            onUpdate: () => {
+                document.getElementById('score').innerHTML = obj.value
+                score = obj.value
+            },
+            onComplete: () => {
+                if (matched == 10) {
+                    alert('You win');
+                } else if (val <= 0) {
+                    alert('You lose');
+                    ready = false
+                }
             }
-
-        }
+        })
     }
+    // onClickCard(cover) {
+
+    //     if (listClick.length >= 2 || cover.isClicked == 1) { return }
+    //     cardFlipAnimate(cover, this.ele.children[0])
+    //     parentNode.push(this);
+    //     listClick.push(cover);
+    //     cover.isClicked = 1;
+    //     if (parentNode.length == 2) {
+    //         ready = false
+    //         if (parentNode[0]._value === parentNode[1]._value) {
+    //             cardZoomOutAnimate(parentNode[0].ele.children[0]);
+    //             cardZoomOutAnimate(parentNode[1].ele.children[0]);
+    //             setTimeout(() => {
+    //                 parentNode[0].active = false;
+    //                 parentNode[1].active = false;
+    //                 parentNode = []; listClick = [];
+    //                 matched++;
+    //                 ready = true
+    //                 console.log('ready')
+    //                 this.setScore(score + 1000)
+    //             }, 2500);
+    //         } else {
+    //             setTimeout(() => {
+    //                 cardFlipAnimate(parentNode[0].ele.children[0], listClick[0])
+    //                 cardFlipAnimate(parentNode[1].ele.children[0], listClick[1], () => {
+    //                     parentNode = []; listClick = []
+    //                     this.setScore(score - 500)
+    //                     console.log('ready')
+    //                     ready = true
+    //                 })
+    //             }, 1000)
+    //         }
+    //     }
+    // }
 }
--- modules/Game.js
+++ modules/Game.js
@@ -1,103 +1,145 @@
 import Node from '../lib/Node.js'
 import { Sprite } from '../lib/Sprite.js'
 import { Card } from './Card.js'
-import {Label} from '../lib/Label.js'
+import { Label } from '../lib/Label.js'
 
-var cardHolder = [
-    {
-        img: './img/trucxanh0.jpg',
-        value: 1,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh1.jpg',
-        value: 2,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh2.jpg',
-        value: 3,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh3.jpg',
-        value: 4,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh4.jpg',
-        value: 5,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh5.jpg',
-        value: 6,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh6.jpg',
-        value: 7,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh7.jpg',
-        value: 8,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh8.jpg',
-        value: 9,
-        available: 2,
-    },
-    {
-        img: './img/trucxanh9.jpg',
-        value: 10,
-        available: 2,
-    },
-]
+var cardHolder = []
+var cardShuffer = []
+let index = 0
+
+for (let i = 0; i < 10; i++) {
+    var arrTemp = []
+    arrTemp.img = './img/trucxanh' + i + '.jpg';
+    arrTemp.value = i + 1;
+    arrTemp.available = 2
+    cardHolder.push(arrTemp)
+}
 
 export class Game extends Node {
-    init() {
-        this._initBackGround()
-        this._initCard()
-        this.initLabel()
-        // var btn =document.createElement('button')
-        // btn.innerHTML='Play again'
-        // btn.addEventListener('click',()=>{location.reload()})
-        // btn.style.zIndex='99'
-        // btn.style.position='relative'
-        // document.body.appendChild(btn)
+    constructor() {
+        super();
+        this._ready = false
+        this._listClick = [];
     }
-    initLabel(){
-        var label=new Label('Score: ',50,'red')
-        this.addChild(label)
-        var score = new Label(10000,50)
-        score.x=150
-        score.ele.id="score"
-        this.addChild(score)
+    get listClick() {
+        return this._listClick
+    }
+    set listClick(val) {
+        this._listClick = val
+    }
+
+    saveClick(val) {
+        this.listClick.push(val)
+    }
+
+    get ready() {
+        return this._ready
+    }
+    set ready(val) {
+        this._ready = val
+        // this.ele.ready=this._ready
+    }
+
+    init() {
+        this._initBackGround();
+        this._initPlayBtn();
+        this.shufferCard();
+        console.log(this.ready);
+    }
+
+    _initPlayBtn() {
+        var btn = new Sprite('./img/play.png');
+        btn.width = 300;
+        btn.height = 200;
+        btn.x = 550
+        btn.y = 350
+        btn.on('mousedown', this.onPlay.bind(this))
+        this.addChild(btn)
+    }
+    onPlay(evt) {
+        this._initCard();
+        this.initLabel();
+        this._initPLayagain();
+    }
+    _initPLayagain() {
+        var playAgain = new Sprite('./img/again.png')
+        playAgain.width = 100;
+        playAgain.height = 100;
+        playAgain.y = 200
+        playAgain.x = 20
+        playAgain.on('mousedown', this.onPlayAgain.bind(this))
+        this.addChild(playAgain)
+
+    }
+    onPlayAgain() {
+        location.reload();
+        // this.shufferCard()
+        // timeline.restart()
+    }
+    initLabel() {
+        var label = new Label('Score: ', 50, 'red');
+        label.y = 50;
+        this.addChild(label);
+        var score = new Label(10000, 50);
+        score.x = 150;
+        score.y = 50;
+        score.ele.id = "score";
+        this.addChild(score);
     }
     _initBackGround() {
         var bg = new Sprite('./img/trucxanh_bg.jpg')
-        bg.width = window.innerWidth;
-        bg.height = window.innerHeight;
-        this.addChild(bg)
+        bg.width = 1500;
+        bg.height = 1000;
+        this.addChild(bg);
     }
+
     _initCard() {
-        var index=0
+        var timeline = gsap.timeline({
+            onComplete: () => {
+                console.log('here')
+                this.ready = true
+            }
+        })
+        this.ele.children[1].style.display = 'none'
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 5; j++) {
                 index++;
-                do {
-                    var randCard = Math.floor(Math.random() * cardHolder.length);
-                } while (cardHolder[randCard].available == 0&&cardHolder[randCard].value!==undefined);
-                cardHolder[randCard].available--;
-                console.log('Shuffle card value'+index+': '+cardHolder[randCard].value)
-                var card = new Card(cardHolder[randCard].img,index,cardHolder[randCard].value);
-                card.x = j * 100;
-                card.y = i * 100;
-                card.ele.id=cardHolder[randCard].value
+                console.log('Shuffle card value' + index + ': ' + cardShuffer[index - 1].value);
+                var card = new Card(cardShuffer[index - 1].img, index, cardShuffer[index - 1].value);
+                gsap.set(card, { x: (j + 2) * 150 + j * 20, y: (i + 1) * 160, opacity: 1 })
+                card.zIndex = 20 - index;
+                card.on('mousedown', this.onClickCard.bind(this, card, index, cardShuffer[index - 1].value))
+                card.ele.id = cardShuffer[index - 1].value;
                 this.addChild(card);
+                timeline.from(card, {
+                    duration: 0.3, x: 650,
+                    y: 350,
+                    zIndex: 99 - index,
+                    ease: "back.inOut(3)",
+                })
             }
         }
+    }
+
+    shufferCard() {
+        cardShuffer = []
+        for (let i = 0; i < 10; i++) {
+            cardHolder[i].available = 2
+        }
+        for (let i = 0; i < 20; i++) {
+            do {
+                var randCard = Math.floor(Math.random() * cardHolder.length);
+                console.log('lol')
+            } while (cardHolder[randCard].available == 0 && cardHolder[randCard].value !== undefined);
+            cardHolder[randCard].available--;
+            cardShuffer.push(cardHolder[randCard])
+        }
+        console.log(cardShuffer)
+    }
+    onClickCard(card, index, value) {
+        if (this.ready == false) return
+        if (this.listClick >= 2 || card.isClicked == 1) return
+        card.isClicked = 1
+        this.saveClick(card)
     }
 }
